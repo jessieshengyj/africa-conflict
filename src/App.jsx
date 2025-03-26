07665b2ed230dd581c5ad2c9c092a1678c5bb2d3
Jessie Sheng <jessieshengyj@gmail.com>
2025-03-25 18:36:45 -0700

    added wendy's plot 1

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ function App() {
                 {/* Sidebar Drawer */}
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                        <li><a className="text-2xl" href="/africa-conflict/">Home</a></li>
-                        <li><a className="text-lg" href="/africa-conflict/#/wendy">Wendy</a></li>
-                        <li><a className="text-lg" href="/africa-conflict/#/jessie">Jessie</a></li>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-60 p-4">
+                        <li><a className="text-xl" href="/africa-conflict/">Home</a></li>
+                        <li><a className="text-md" href="/africa-conflict/#/wendy">Wendy</a></li>
+                        <li><a className="text-md" href="/africa-conflict/#/jessie">Jessie</a></li>
                     </ul>
                 </div>
             </div>
--- src/pages/PersonA.jsx
+++ src/pages/PersonA.jsx
@@ -1,5 +1,17 @@
 function PersonA() {
-    return <h1 className="text-2xl">Analysis for Wendy</h1>;
+    return (
+        <>
+            {/*<h1 className="text-2xl">Analysis for Wendy</h1>*/}
+            <div className="iframe-container">
+                <iframe
+                    src="/africa-conflict/charts/plot1.html"
+                    width="1200"
+                    height="1350"
+                    frameBorder="0"
+                ></iframe>
+            </div>
+        </>
+    );
 }
 
 export default PersonA;
